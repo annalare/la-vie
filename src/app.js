877cc8952d14e2cd034311fdab439aadf2512791
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-07-23 16:49:12 -0300

    feat : conexão com o banco

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const db = require("./database");
+const routes = express.Router();
+
 const app = express();
 
+db.hasConnection();
+
 app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
